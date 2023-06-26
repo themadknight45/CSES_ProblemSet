e747acbb267af5581b467e4b47196fad83dd33fe
themadknight45 <rishirajssnl@gmail.com>
2023-06-26 06:09:53 +0530

    GridPath

--- Removing Digits/Remdigit.cpp
+++ Removing Digits/Remdigit.cpp
@@ -2,29 +2,30 @@
 #include <algorithm>
 #include <bits/stdc++.h>
 #include <vector>
+#include <iostream>
+#include <string>
 using namespace std;
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 vector<long long>graph[200007];int mod=1e9+7;
-int f(int n,int dp[]){
-    // cout<<n<<"\n";
-    if(n==0)return 0;
-    if(n<=9)return 1;
-    if(n<0)return INT32_MAX;
-    string s=to_string(n);
-    if(dp[n]!=-1)return dp[n];
-    int mini=INT32_MAX;
-    for(char x:s){
-        int tmp=x-'0';
-        if(tmp!=0)
-         mini=min(mini,1+f(n-tmp,dp));
-    }
-    return dp[n]=mini;
-}
+// int dp[1000][1000];
 void solve(int test){
-    int n;cin>>n;
-		int dp[n+1];
-        for(int i=0;i<=n;i++)dp[i]=-1;
-		cout<<f(n,dp);
+    int n;
+    cin>>n;
+    string str[n];
+    for(int i=0;i<n;i++){
+        string s; getline(cin, s);
+        str[i]=s;
+    }
+    int dp[n][str[0].length()];
+    for(int i=0;i<n;i++)for(int j=0;j<str[0].length();j++)dp[i][j]=-1;
+
+}
+int dfs(int i,int j,vector<vector<int>>dp,string str[]){
+    if(i<0||j<0||i>=dp.size()||j>=dp[0].size()){
+        return 1e9;
+    }
+    if(i==dp.size()-1&&j==dp[0].size()-1){return 0;}
+    int min=
 }
 int main(){
     fast;
